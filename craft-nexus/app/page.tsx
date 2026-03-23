afb69928ac97be7c350ae71a4830a063cdd3eac0
Agbeleshe <danielagbeleshe@gmail.com>
2026-03-23 08:05:02 +0100

    local changes

--- craft-nexus/app/page.tsx
+++ craft-nexus/app/page.tsx
@@ -3,21 +3,14 @@ import FeatureBar from "@/components/organisms/FeatureBar";
 import FeaturedCourses from "@/components/organisms/FeaturedCourses";
 import HeroSection from "@/components/organisms/HomepageHero";
 import TestimonialsSection from "@/components/organisms/Testimonials";
-<<<<<<< HEAD
-import FeatureBar from "@/components/organisms/FeatureBar";
-=======
 import TopCourses from "@/components/organisms/TopCourses";
->>>>>>> a1aa3f3b1c36a01b32b745569cc8a0acd76dff66
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
-<<<<<<< HEAD
-=======
       <CategorySection />
       <TopCourses />
->>>>>>> a1aa3f3b1c36a01b32b745569cc8a0acd76dff66
       <FeatureBar />
       <TestimonialsSection />
       <FeaturedCourses />
